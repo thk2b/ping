5c586e464f2dcdaf56f49598435b815e115669be
thk2b <theokobb@gmail.com>
2019-05-22 23:27:01 -0700

    use sendmsg

--- src/icmp_socket.c
+++ src/icmp_socket.c
@@ -34,6 +34,30 @@ int icmp_socket__recv(
     void *buf,
     size_t size
 ) {
+    struct iovec vec = {
+        .iov_base = buf,
+        .iov_len = size
+    };
+    struct msghdr hdr = {0};
+    char ctrlbuf[1000] = {0};
+    hdr.msg_name = NULL;
+    hdr.msg_namelen = 0;
+    hdr.msg_iov = &vec;
+    hdr.msg_iovlen = 1;
+    hdr.msg_control = ctrlbuf;
+    hdr.msg_controllen = 1000;
+    ssize_t recieved = recvmsg(s, &hdr, 0);
+    if (recieved < 0) {
+        return 1;
+    }
+    return 0;
+}
+/*
+int icmp_socket__recv(
+    icmpsock_t s,
+    void *buf,
+    size_t size
+) {
     socklen_t addrlen;
     struct sockaddr_in from;
     ssize_t recieved = recvfrom(s, buf, size, 0, (struct sockaddr*)&from, &addrlen);
@@ -42,3 +66,4 @@ int icmp_socket__recv(
     }
     return 0;
 }
+*/
